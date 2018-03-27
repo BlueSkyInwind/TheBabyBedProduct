891b7defea2225d7c2f159d6a45317d8b841eebf
chinesemanbobo <chinesemanbobo@163.com>
2018-03-27 15:53:37 +0800

    移除空文件 再次提交test

---
+++ TheBabyBedProduct/Modules/MyModules/Setting/WarningRingSetting/BBWarningRingListCell.h
@@ -0,0 +1,13 @@
+//
+//  BBWarningRingListCell.h
+//  TheBabyBedProduct
+//
+//  Created by ╰莪呮想好好宠Nǐつ on 2018/3/27.
+//  Copyright © 2018年 Wangyongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBWarningRingListCell : UITableViewCell
+-(void)setupCellWithRingName:(NSString *)ringName isSelected:(BOOL)isSelected;
+@end
