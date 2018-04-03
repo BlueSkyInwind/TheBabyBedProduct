a534227bcd8cb8fdde9f416397f2ae12550a2637
chinesemanbobo <chinesemanbobo@163.com>
2018-04-03 22:48:40 +0800

    我的设备

--- TheBabyBedProduct/Modules/MyModules/MyDevice/BBMyDeviceViewController.m
+++ TheBabyBedProduct/Modules/MyModules/MyDevice/BBMyDeviceViewController.m
@@ -9,36 +9,103 @@
 #import "BBMyDeviceViewController.h"
 
 @interface BBMyDeviceViewController ()
-
+@property(nonatomic,strong) UITableView *tableView;
+/** 设备信息title数组 */
+@property(nonatomic,strong) NSMutableArray *deviceMessageTitles;
+/** 已连接的传感器数组 */
+@property(nonatomic,strong) NSMutableArray *sensors;
 @end
 
 @implementation BBMyDeviceViewController
 
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    [self.navigationController setNavigationBarHidden:NO animated:animated];
-}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"我的小雅智能";
     self.view.backgroundColor = k_color_vcBg;
+    
+//    [self creatUI];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)backAction
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+-(void)creatUI
+{
+    UIImageView *topImgBgV = [UIImageView bb_imgVMakeWithSuperV:self.view imgName:@"console_header_Icon"];
+    topImgBgV.frame = CGRectMake(0, 0, _k_w, 436);
+    topImgBgV.userInteractionEnabled = YES;
+    
+    UIButton *backBt = [UIButton bb_btMakeWithSuperV:topImgBgV imageName:@"cryingChooseDate_left_Icon"];
+    backBt.frame = CGRectMake(0, 20, 50, 44);
+    [backBt addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
+    
+    CGFloat tableY = topImgBgV.bottom-50;
+    self.tableView = [UITableView bb_tableVMakeWithSuperV:self.view frame:CGRectMake(0, tableY, _k_w, _k_h-tableY) delegate:self bgColor:k_color_vcBg style:UITableViewStylePlain];
+}
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning todo 没有绑定为1
+    return 2;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return self.deviceMessageTitles.count;
+    }else if (section == 1){
+        return self.sensors.count;
+    }
+    return 0;
+}
+//-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    
+//}
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 50;
+}
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *headerV = [[UIView alloc]initWithFrame:CGRectMake(0, 0, _k_w, 50)];
+    headerV.backgroundColor = [UIColor clearColor];
+    UILabel *titleLB = [UILabel bb_lbMakeWithSuperV:headerV fontSize:16 alignment:NSTextAlignmentLeft textColor:rgb(102, 102, 102, 1)];
+    titleLB.frame = CGRectMake(10, 10, _k_w-20, 40);
+    titleLB.backgroundColor = [UIColor clearColor];
+    
+#warning todo
+    if (section == 0) {
+        titleLB.text = @"基本信息";
+    }else{
+        titleLB.text = @"已绑定传感器";
+    }
+    
+    return headerV;
+}
+-(UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    return [UIView new];
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.0001;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(NSMutableArray *)deviceMessageTitles
+{
+    if (!_deviceMessageTitles) {
+        _deviceMessageTitles = [NSMutableArray array];
+    }
+    return _deviceMessageTitles;
 }
-*/
+-(NSMutableArray *)sensors
+{
+    if (!_sensors) {
+        _sensors = [NSMutableArray array];
+    }
+    return _sensors;
+}
 
 @end
